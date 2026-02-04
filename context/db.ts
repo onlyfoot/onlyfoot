4657e2260be263082d48aef18b3e8ee226fb3176
onlyfoot <onlyfoot.agency@gmail.com>
2026-02-04 19:19:55 -0300

    Update db.ts

--- context/db.ts
+++ context/db.ts
@@ -1,7 +1,7 @@
 import { Pool } from 'pg';
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL, // configure no Render
+  connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
 });
 
