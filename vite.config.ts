25cb129a8fc42099ed3975791332e7dcf1794ace
onlyfoot <onlyfoot.agency@gmail.com>
2026-02-04 19:03:06 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,5 +11,10 @@ export default defineConfig({
   server: {
     port: 5173,
     open: true
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT ? parseInt(process.env.PORT) : 4173,
+    allowedHosts: ['onlyfoot.onrender.com']
   }
 })
