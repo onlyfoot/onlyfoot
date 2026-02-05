2351e00102d43394964f2b28102a8023156be8d9
onlyfoot <onlyfoot.agency@gmail.com>
2026-02-05 12:27:42 -0300

    Create index.tsx

---
+++ components/index.tsx
@@ -0,0 +1,6 @@
+// components/index.tsx
+import AuthLayout from './AuthLayout';
+import Login from '../pages/Login';
+import Register from '../pages/Register';
+
+export { AuthLayout, Login, Register };
